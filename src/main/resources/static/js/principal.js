12e950fe84a25b0293e82df19a8cf3331a79d37a
leandro91004850 <leandroleekrap@gmail.com>
2020-03-15 01:38:37 -0300

    manipulando os dados com operacoes logicas

--- src/main/resources/static/js/principal.js
+++ src/main/resources/static/js/principal.js
@@ -4,17 +4,34 @@ var titulo = document.querySelector(".titulo");
 var paciente = document.querySelector("#primeiro-paciente");
 
 	var tdpeso = paciente.querySelector(".info-peso");
-	var peso = tdpeso.textContent; // capiturar somente o valor
+	var peso = tdpeso.textContent; 
 
 	var tdAltura = paciente.querySelector(".info-altura");
 	var altura = tdAltura.textContent;
 
-	var imc = peso / (altura * altura); // () determina os quais operadores serao executados primeiro
+	var tdImc = paciente.querySelector(".info-imc");
 
-	console.log(imc);
+	var pesoValido = true;
+	var alturaValida = true;
+
+	if(peso <= 0 || peso >= 1000){
+		console.log("peso invalido");
+		pesoValido = false;
+		tdImc.textContent = "peso inválido";
+	}
+
+	if(altura <= 0 || altura >= 3.00){
+		console.log("altura invalida");
+		alturaValida = false;
+		tdImc.textContent="altura inválida";
+	}
 
 
-/*
-	console.log(paciente); //tr
-	console.log(tdpeso); // td aonde esta amarzedo o peso do paciente
-	console.log(peso);*/
+	if(alturaValida && pesoValido){ // caso os dois sejam verdadeiros
+		var imc = peso / (altura * altura); 
+		tdImc.textContent = imc;
+	
+	}
+
+
+
